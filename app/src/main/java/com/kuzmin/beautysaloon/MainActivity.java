70dd73753a2762697c3a88caed42262ba6441b3d
yurii_kuzmin <poshta.kuzmin@gmail.com>
2022-11-08 14:44:11 +0300

    подключение аутентификации

--- app/src/main/java/com/kuzmin/beautysaloon/MainActivity.java
+++ app/src/main/java/com/kuzmin/beautysaloon/MainActivity.java
@@ -2,18 +2,35 @@ package com.kuzmin.beautysaloon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgPas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imgPas=(ImageView) findViewById(R.id.background_main);
+
         Window w= getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        imgPas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AuthActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
