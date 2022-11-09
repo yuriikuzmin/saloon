ce7a871fbad74b396d4e1b919f81feff21dcd6e5
yurii_kuzmin <poshta.kuzmin@gmail.com>
2022-11-09 15:38:38 +0300

    закончено AuthActivity

--- app/src/main/java/com/kuzmin/beautysaloon/AuthActivity.java
+++ app/src/main/java/com/kuzmin/beautysaloon/AuthActivity.java
@@ -2,6 +2,7 @@ package com.kuzmin.beautysaloon;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -25,9 +26,10 @@ public class AuthActivity extends AppCompatActivity {
 
     private EditText edLogin, edPassword;
     private FirebaseAuth mAuth;
-    FirebaseUser user;
-    private TextView tvUserTel;
-    private Button button_reg, button_exit, bStart, button_in;
+    public static FirebaseUser user;
+    private TextView tvUserEmail;
+    private Button button_in, button_reg, button_add_clint, button_find_client, button_exit;
+    public static String textEmail;
 
 
     @Override
@@ -39,13 +41,33 @@ public class AuthActivity extends AppCompatActivity {
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         init();
-        button_in.setOnClickListener(new View.OnClickListener() {
+        onStart();
+        button_in.setOnClickListener(new View.OnClickListener() {//кнопка входа после вводе логина и пароля
             @Override
             public void onClick(View v) {
                 singIn();
             }
         });
-        button_reg.setOnClickListener(new View.OnClickListener() {
+        button_reg.setOnClickListener(new View.OnClickListener() {//кнопка регистрации после ввода логина и пароля
+            @Override
+            public void onClick(View v) {
+                singUp();
+            }
+        });
+        button_exit.setOnClickListener(new View.OnClickListener() {//кнопка выхода из аккаунта
+            @Override
+            public void onClick(View v) {
+                user=null;
+                showSigNot();
+            }
+        });
+        button_add_clint.setOnClickListener(new View.OnClickListener() {//кнопка добавить клиента после входа в аккаунт
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        button_find_client.setOnClickListener(new View.OnClickListener() {//кнопка найти клиента после входа в аккаунт
             @Override
             public void onClick(View v) {
 
@@ -56,10 +78,14 @@ public class AuthActivity extends AppCompatActivity {
     private void  init(){
         edLogin = (EditText)findViewById(R.id.edit_login);
         edPassword = (EditText) findViewById(R.id.edit_name_kod);
+        tvUserEmail=(TextView)findViewById(R.id.text_auth_enter);
         mAuth = FirebaseAuth.getInstance();
         button_in=(Button) findViewById(R.id.button_enter);
         button_reg=(Button) findViewById(R.id.button_registration);
-        user = mAuth.getCurrentUser();
+        button_add_clint=(Button) findViewById(R.id.button_add_client);
+        button_find_client=(Button)findViewById(R.id.button_find_client);
+        button_exit=(Button)findViewById(R.id.button_exit);
+        user = null;
     }
 
     private void singIn(){
@@ -68,7 +94,7 @@ public class AuthActivity extends AppCompatActivity {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()) {
-
+                        singInVer();
 
                         Toast.makeText(AuthActivity.this, "Пользователь определен, вход выполнен", Toast.LENGTH_SHORT).show();
                     } else {
@@ -80,6 +106,95 @@ public class AuthActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Введите логин и пароль", Toast.LENGTH_SHORT).show();
         }
+    }
+    private void showSigIn(){ //дисплей после успешного входа в аккаунт
+        tvUserEmail.setVisibility(View.VISIBLE);
+        button_add_clint.setVisibility(View.VISIBLE);
+        button_exit.setVisibility(View.VISIBLE);
+        button_find_client.setVisibility(View.VISIBLE);
+        edLogin.setVisibility(View.GONE);
+        edPassword.setVisibility(View.GONE);
+        button_reg.setVisibility(View.GONE);
+        button_in.setVisibility(View.GONE);
+    }
+    private void showSigNot(){ //дисплей если вход в аккаунт не выполнен
+        tvUserEmail.setVisibility(View.VISIBLE);
+        button_add_clint.setVisibility(View.GONE);
+        button_exit.setVisibility(View.GONE);
+        button_find_client.setVisibility(View.GONE);
+        edLogin.setVisibility(View.VISIBLE);
+        edPassword.setVisibility(View.VISIBLE);
+        button_reg.setVisibility(View.VISIBLE);
+        button_in.setVisibility(View.VISIBLE);
+        tvUserEmail.setText(R.string.text_auth);
+        edLogin.setText(null);
+        edPassword.setText(null);
+    }
+    private void sendEmailVer(){//Проверка существования E-mail пользователя
+        user=mAuth.getCurrentUser();
+        assert user!=null;
+        user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(AuthActivity.this, "Проверьте вашу E-mail почту для подтверждения адреса", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(AuthActivity.this, "E-mail не верный, проверьте вашу запись", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+    private void singInVer(){
+        user=mAuth.getCurrentUser();
+        assert user!=null;
+        if(user.isEmailVerified()){
+            showSigIn();
+            tvUserEmail.setText(user.getEmail());
+            textEmail=tvUserEmail.getText().toString();
+            Log.d("LOG", "рез E-mail"+textEmail);
+        }
+    }
+
+    public void singUp(){
+        if(!TextUtils.isEmpty(edLogin.getText().toString())&&(!TextUtils.isEmpty(edPassword.getText().toString()))){
+            mAuth.createUserWithEmailAndPassword(edLogin.getText().toString(), edPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Log.d("LOG", "Проверка результата регистрации");
+                        sendEmailVer();
+                        Log.d("LOG", "Проверка результата E-mail");
+                        showSigIn();
+                        singInVer();
+                        Toast.makeText(AuthActivity.this, "Пользователь зарегестрирован успешно", Toast.LENGTH_SHORT).show();
+                    } else {
+                        showSigNot();
+                        Toast.makeText(AuthActivity.this, "Регистрация не выполнена", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            });
+
+        }else {
+            Toast.makeText(this, "Введите логин и пароль", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (user!= null) {
+            showSigIn();
+        } else {
+            showSigNot();
+        }
+
+    }
+    public String getTextEmail() {
+        Log.d("LOG", "рез E-mail get"+textEmail);
+        return textEmail;
     }
 
 
