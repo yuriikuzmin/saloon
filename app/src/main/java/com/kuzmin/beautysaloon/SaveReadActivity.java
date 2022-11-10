374f07c2ebcfac761065c3433b8dc5fdf500be51
yurii_kuzmin <poshta.kuzmin@gmail.com>
2022-11-10 13:21:19 +0300

    SaveReadActivity готова

--- app/src/main/java/com/kuzmin/beautysaloon/SaveReadActivity.java
+++ app/src/main/java/com/kuzmin/beautysaloon/SaveReadActivity.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -32,7 +33,7 @@ import java.util.concurrent.TimeUnit;
 
 public class SaveReadActivity extends AppCompatActivity {
 
-    ImageView img_photo_clint;
+    ImageView img_photo_client;
     EditText edNameClient, edNameSecondClient, edTelClient;
     Button btn_add_photo, btn_save_client, btn_read_base;
     private String SALOON_KEY="SALOON";
@@ -59,15 +60,14 @@ public class SaveReadActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 getPhoto();
+                uploadPhoto();
             }
         });
 
         btn_save_client.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uploadPhoto();
                 saveClient();
-
             }
         });
 
@@ -80,7 +80,7 @@ public class SaveReadActivity extends AppCompatActivity {
 
     }
     public void init(){
-        img_photo_clint=(ImageView)findViewById(R.id.image_photo_client);
+        img_photo_client=(ImageView)findViewById(R.id.image_photo_client);
         edNameClient=(EditText) findViewById(R.id.edit_name_client);
         edNameSecondClient=(EditText) findViewById(R.id.edit_secondName_client);
         edTelClient=(EditText) findViewById(R.id.edit_phone_client);
@@ -95,19 +95,23 @@ public class SaveReadActivity extends AppCompatActivity {
         intentChooser.setType("image/*");//определяем тим сущности структуру пути к картинке
         intentChooser.setAction(Intent.ACTION_GET_CONTENT);//определяем назначение этой сущности
         startActivityForResult(intentChooser, 1);//запускаем активность с этой сущностью, присваиваем код запроса
+        Log.d("LOG", "Картинку выбрали");
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {//Загрузка выбраного фото в окно
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==1 && data!=null && data.getData()!=null){
-            if(requestCode==RESULT_OK){
-                img_photo_clint.setImageURI(data.getData());
+        Log.d("LOG", "requestCode = " + requestCode + ", resultCode = " + resultCode);
+        if(requestCode==1 && data!=null && data.getData()!=null)
+        {
+            if(resultCode==RESULT_OK ){
+                img_photo_client.setImageURI(data.getData());
             }
         }
     }
 
     private void uploadPhoto(){
-        Bitmap bitmap=((BitmapDrawable)img_photo_clint.getDrawable()).getBitmap();
+        Bitmap bitmap=((BitmapDrawable)img_photo_client.getDrawable()).getBitmap();
         ByteArrayOutputStream baos=new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG,100, baos);
         byte[]byteArray=baos.toByteArray();
@@ -122,6 +126,8 @@ public class SaveReadActivity extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<Uri> task) {
                 uploadUri=task.getResult();
+                Log.d("LOG", "Ссылка на storage"+uploadUri.toString());
+
 
             }
         });
@@ -130,15 +136,22 @@ public class SaveReadActivity extends AppCompatActivity {
     private void saveClient() {
         String id = myRef.push().getKey();
         String name = edNameClient.getText().toString();
-        String second_name = edNameSecondClient.getText().toString();
+        String sec_name = edNameSecondClient.getText().toString();
         String tel = edTelClient.getText().toString();
-        Client newClient = new Client(id, name, second_name, tel, uploadUri.toString());
-        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(second_name) && !TextUtils.isEmpty(tel)) {
+        Client newClient = new Client(id, name, sec_name, tel, uploadUri.toString());
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(sec_name) && !TextUtils.isEmpty(tel)) {
 
             if (id != null) {
                 myRef.child(id).setValue(newClient);
+                Toast.makeText(this, "Запись нового пользователя сохранена", Toast.LENGTH_SHORT).show();
+                edNameClient.setText("");
+                edNameSecondClient.setText("");
+                edTelClient.setText("");
+                img_photo_client.setImageResource(R.drawable.three_level2);
+
+
             }
-            Toast.makeText(this, "Запись нового пользователя сохранена", Toast.LENGTH_SHORT).show();
+
         } else {
             Toast.makeText(this, "Заполните поля", Toast.LENGTH_SHORT).show();
         }
